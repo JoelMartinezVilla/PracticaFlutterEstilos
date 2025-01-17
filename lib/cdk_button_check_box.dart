820968d0c9458fb82b145faba71b37231a1f49b6
Daniel Artiaga Perez <dartiagaperez.cf@iesesteveterradas.cat>
2025-01-17 16:14:03 +0100

    checkBox Button Hecho
    Estilo WindowsXP

--- lib/cdk_button_check_box.dart
+++ lib/cdk_button_check_box.dart
@@ -63,116 +63,50 @@ class CDKButtonCheckBoxPainter extends CustomPainter {
     required this.isLightTheme,
   });
 
-  /// Draws a shadow around the checkbox to provide visual depth
-  void drawShadow(Canvas canvas, Size size, Rect rect) {
-    /// Define the path for the rounded square
-    Path squarePath = Path()
-      ..addRRect(RRect.fromRectAndRadius(rect, const Radius.circular(4)));
-
-    /// Restrict the shadow painting to the checkbox area
-    canvas.clipPath(squarePath);
-
-    /// Create a Paint object for the shadow
-    Paint shadowPaint = Paint();
-
-    /// Offset the shadow based on the theme
-    Offset shadowOffset = const Offset(0, 0);
-    if (isLightTheme) {
-      shadowOffset = const Offset(0, -10);
-      shadowPaint = Paint()
-        ..color = CDKTheme.black.withOpacity(0.25)
-        ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 10);
-    } else {
-      shadowOffset = const Offset(0, -8);
-      shadowPaint = Paint()
-        ..color = CDKTheme.black.withOpacity(0.5)
-        ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 6);
-    }
-
-    // Draw shadow
-    canvas.drawRRect(
-        RRect.fromRectAndRadius(
-            rect.shift(shadowOffset), const Radius.circular(4)),
-        shadowPaint);
-
-    // Restore drawing clip
-    canvas.clipRect(Rect.fromLTWH(0, 0, size.width, size.height));
-  }
-
   @override
   void paint(Canvas canvas, Size size) {
-    double borderRadius = 4.0;
     Rect squareRect = Rect.fromCenter(
-        center: Offset(size.width / 2, size.height / 2),
-        width: size.width,
-        height: size.height);
-    RRect roundedSquare =
-        RRect.fromRectAndRadius(squareRect, Radius.circular(borderRadius));
+      center: Offset(size.width / 2, size.height / 2),
+      width: size.width,
+      height: size.height,
+    );
+
     Paint paint = Paint();
 
-    if (isSelected && hasAppFocus) {
-      LinearGradient gradient = LinearGradient(
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        colors: [colorAccent200, colorAccent],
-      );
-
-      paint = Paint()
-        ..style = PaintingStyle.fill
-        ..shader = gradient.createShader(squareRect);
-
-      canvas.drawRRect(roundedSquare, paint);
-    } else {
-      // Draw background square & shadow
-      paint = Paint()
-        ..style = PaintingStyle.fill
-        ..color = colorBackgroundSecondary0;
-      canvas.drawRRect(roundedSquare, paint);
-
-      drawShadow(canvas, size, squareRect);
-    }
-
-    // Draw outer square
-    Color outerColor = CDKTheme.grey;
-    if (!isLightTheme) {
-      outerColor = CDKTheme.grey600;
-    }
+    // Fondo blanco
     paint = Paint()
+      ..style = PaintingStyle.fill
+      ..color = const Color(0xFFFFFFFF); // Blanco puro
+    canvas.drawRect(squareRect, paint);
+
+    // Borde azul
+    Paint borderPaint = Paint()
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 1
-      ..color = outerColor;
+      ..strokeWidth = 1.5
+      ..color = const Color(0xFF003CA5); // Azul estilo XP
+    canvas.drawRect(squareRect, borderPaint);
 
-    canvas.drawRRect(roundedSquare, paint);
-
+    // Tick verde
     if (isSelected) {
-      Color selectColor = CDKTheme.white;
-      if (isLightTheme && !hasAppFocus) {
-        selectColor = CDKTheme.black;
-      }
-      Paint linePaint = Paint()
-        ..color = selectColor
+      Paint checkPaint = Paint()
+        ..color = const Color(0xFF00FF00) // Verde para el tick
         ..strokeWidth = 2.0
-        ..style = PaintingStyle.stroke;
+        ..style = PaintingStyle.stroke
+        ..strokeCap = StrokeCap.round;
 
-      // First line coordinates
-      Offset startLine1 = Offset(size.width * 0.15, size.height * 0.48);
-      Offset endLine1 = Offset(size.width * 0.38, size.height * 0.80);
-      canvas.drawLine(startLine1, endLine1, linePaint);
+      // Coordenadas del tick
+      Offset startLine1 = Offset(size.width * 0.20, size.height * 0.50);
+      Offset endLine1 = Offset(size.width * 0.40, size.height * 0.75);
+      Offset startLine2 = Offset(size.width * 0.40, size.height * 0.75);
+      Offset endLine2 = Offset(size.width * 0.80, size.height * 0.25);
 
-      // Second line coordinates
-      Offset startLine2 = Offset(size.width * 0.36, size.height * 0.80);
-      Offset endLine2 = Offset(size.width * 0.75, size.height * 0.22);
-      canvas.drawLine(startLine2, endLine2, linePaint);
+      canvas.drawLine(startLine1, endLine1, checkPaint);
+      canvas.drawLine(startLine2, endLine2, checkPaint);
     }
   }
 
   @override
   bool shouldRepaint(covariant CDKButtonCheckBoxPainter oldDelegate) {
-    return oldDelegate.colorAccent != colorAccent ||
-        oldDelegate.colorAccent200 != colorAccent200 ||
-        oldDelegate.colorBackgroundSecondary0 != colorBackgroundSecondary0 ||
-        oldDelegate.isSelected != isSelected ||
-        oldDelegate.hasAppFocus != hasAppFocus ||
-        oldDelegate.size != size;
+    return oldDelegate.isSelected != isSelected;
   }
 }

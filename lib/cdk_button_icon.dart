63a7387bbcef2bee8953a5f2442121b911faba7f
Daniel Artiaga Perez <dartiagaperez.cf@iesesteveterradas.cat>
2025-02-13 18:09:18 +0100

    algo nuevo??

--- lib/cdk_button_icon.dart
+++ lib/cdk_button_icon.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'cdk_theme_notifier.dart';
 import 'cdk_theme.dart';
+import 'package:flutter/material.dart';
 
-// Copyright © 2023 Albert Palacios. All Rights Reserved.
+// Copyright Â© 2023 Albert Palacios. All Rights Reserved.
 // Licensed under the BSD 3-clause license, see LICENSE file for details.
 
 class CDKButtonIcon extends StatefulWidget {
@@ -28,6 +29,7 @@ class CDKButtonIcon extends StatefulWidget {
 class CDKButtonIconState extends State<CDKButtonIcon> {
   bool _isPressed = false;
   bool _isHovering = false;
+
   void _onTapDown(TapDownDetails details) {
     setState(() => _isPressed = true);
   }
@@ -52,28 +54,24 @@ class CDKButtonIconState extends State<CDKButtonIcon> {
   Widget build(BuildContext context) {
     CDKTheme theme = CDKThemeNotifier.of(context)!.changeNotifier;
 
-    final Color backgroundColor = theme.isLight
-        ? _isPressed
-            ? CDKTheme.grey70
-            : _isHovering
-                ? CDKTheme.grey50
-                : widget.isSelected
-                    ? theme.backgroundSecondary1
+    final Color backgroundColor = widget.isSelected
+        ? (_isPressed ? Colors.red.shade900 : Colors.red)
+        : theme.isLight
+            ? _isPressed
+                ? CDKTheme.grey70
+                : _isHovering
+                    ? CDKTheme.grey50
                     : CDKTheme.transparent
-        : _isPressed
-            ? CDKTheme.grey
-            : _isHovering
-                ? CDKTheme.grey600
-                : widget.isSelected
-                    ? theme.backgroundSecondary1
+            : _isPressed
+                ? CDKTheme.grey
+                : _isHovering
+                    ? CDKTheme.grey600
                     : CDKTheme.transparent;
 
-    final Color textColor = theme.isLight
-        ? widget.isSelected && theme.isAppFocused
-            ? theme.accent
-            : theme.colorText
-        : widget.isSelected && theme.isAppFocused
-            ? CDKTheme.white
+    final Color textColor = widget.isSelected
+        ? Colors.white
+        : theme.isLight
+            ? theme.colorText
             : theme.colorText;
 
     return MouseRegion(
@@ -84,35 +82,32 @@ class CDKButtonIconState extends State<CDKButtonIcon> {
         onTapUp: _onTapUp,
         onTapCancel: _onTapCancel,
         onTap: widget.onPressed,
-        child: widget.isCircle
-            ? DecoratedBox(
-                decoration: BoxDecoration(
-                    color: backgroundColor,
-                    borderRadius: BorderRadius.circular(widget.size)),
-                child: Container(
-                    width: widget.size,
-                    height: widget.size,
-                    alignment: Alignment.center,
-                    child: Icon(
-                      widget.icon,
-                      color: textColor,
-                      size: widget.size * 0.5, // Icona més petita que el botó
-                    )),
-              )
-            : DecoratedBox(
-                decoration: BoxDecoration(
-                    color: backgroundColor,
-                    borderRadius: BorderRadius.circular(8)),
-                child: Container(
-                    width: widget.size,
-                    height: widget.size,
-                    alignment: Alignment.center,
-                    child: Icon(
-                      widget.icon,
-                      color: textColor,
-                      size: widget.size * 0.75,
-                    )),
-              ),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+                color: widget.isSelected ? Colors.blue : Colors.transparent,
+                width: 2),
+            gradient: widget.isSelected
+                ? LinearGradient(
+                    colors: _isPressed
+                        ? [Colors.red.shade900, Colors.red.shade700]
+                        : [Colors.red.shade700, Colors.redAccent],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  )
+                : null,
+          ),
+          child: Container(
+            width: widget.size,
+            height: widget.size,
+            alignment: Alignment.center,
+            child: widget.isSelected
+                ? Icon(Icons.close,
+                    color: Colors.white, size: widget.size * 0.75)
+                : Icon(widget.icon, color: textColor, size: widget.size * 0.75),
+          ),
+        ),
       ),
     );
   }
